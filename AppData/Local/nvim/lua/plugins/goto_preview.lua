8b9508e8023fc1b97f8d0610493aeb9903d5c0e4
felix <Wu-Felix@outlook.com>
2024-11-18 13:39:28 +0800

    refactor(nvim):goto_preview trailblazer 按照lazy.nvim 重新构造

--- AppData/Local/nvim/lua/plugins/goto_preview.lua
+++ AppData/Local/nvim/lua/plugins/goto_preview.lua
@@ -2,26 +2,40 @@
 return {
   "rmagatti/goto-preview",
   lazy = true,
-  keys = { "gp" },
-  config = function()
-    require("goto-preview").setup({
-      width = 120,
-      height = 25,
-      default_mappings = true,
-      debug = false,
-      opacity = nil,
-      post_open_hook = nil,
-      vim.cmd("nnoremap <leader>gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>"),
-      vim.cmd("nnoremap <leader>gpt <cmd>lua require('goto-preview').goto_preview_type_definition()<CR>"),
-      vim.cmd("nnoremap <leader>gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>"),
-      vim.cmd("nnoremap <leader>gpD <cmd>lua require('goto-preview').goto_preview_declaration()<CR>"),
-      vim.cmd("nnoremap <leader>gP <cmd>lua require('goto-preview').close_all_win()<CR>"),
-      vim.cmd("nnoremap <leader>gpr <cmd>lua require('goto-preview').goto_preview_references()<CR>"),
-      -- You can use "default_mappings = true" setup option
-      -- Or explicitly set keybindings
-      -- vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
-      -- vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
-      -- vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
-    })
-  end,
+  keys = {
+    {
+      "<leader>gpd",
+      "<cmd>lua require('goto-preview').goto_preview_definition()<CR>",
+      desc = "goto_preview_definition",
+    },
+    {
+      "<leader>gpt",
+      "<cmd>lua require('goto-preview').goto_preview_type_definition()<CR>",
+      desc = "goto_preview_type_definition",
+    },
+    {
+      "<leader>gpi",
+      "<cmd>lua require('goto-preview').goto_preview_implementation()<CR>",
+      desc = "goto_preview_implementation",
+    },
+    {
+      "<leader>gpD",
+      "<cmd>lua require('goto-preview').goto_preview_declaration()<CR>",
+      desc = "goto_preview_declaration",
+    },
+    { "<leader>gP", "<cmd>lua require('goto-preview').close_all_win()<CR>", desc = "close_all_win" },
+    {
+      "<leader>gpr",
+      "<cmd>lua require('goto-preview').goto_preview_references()<CR>",
+      desc = "goto_preview_references",
+    },
+  },
+  opts = {
+    width = 80,
+    height = 20,
+    default_mappings = true,
+    debug = false,
+    opacity = nil,
+    post_open_hook = nil,
+  },
 }
--- AppData/Local/nvim/lua/plugins/trailblazer.lua
+++ AppData/Local/nvim/lua/plugins/trailblazer.lua
@@ -3,62 +3,70 @@ return {
   {
     "LeonHeidelbach/trailblazer.nvim",
     lazy = true,
-    keys = { "<A-s>", "<A-d>" },
-    config = function()
-      -- local HOME = os.getenv("HOME")
-      require("trailblazer").setup({
-        auto_save_trailblazer_state_on_exit = false,
-        auto_load_trailblazer_state_on_enter = false,
-        -- custom_session_storage_dir = HOME .. "/.local/share/trail_blazer_sessions/",
-        trail_options = {
-          mark_symbol = "•", --  will only be used if trail_mark_symbol_line_indicators_enabled = true
-          newest_mark_symbol = "󰝥", -- disable this mark symbol by setting its value to ""
-          cursor_mark_symbol = "󰺕", -- disable this mark symbol by setting its value to ""
-          next_mark_symbol = "󰬦", -- disable this mark symbol by setting its value to ""
-          previous_mark_symbol = "󰬬", -- disable this mark symbol by setting its value to ""
-        },
-        mappings = {
-          nv = {
-            motions = {
-              new_trail_mark = "<A-s>",
-              track_back = "<A-d>",
-              peek_move_next_down = "<A-J>",
-              peek_move_previous_up = "<A-K>",
-              move_to_nearest = "<A-n>",
-              toggle_trail_mark_list = "<A-o>",
-            },
-            actions = {
-              delete_all_trail_marks = "<A-L>",
-              paste_at_last_trail_mark = "<A-p>",
-              paste_at_all_trail_marks = "<A-P>",
-              set_trail_mark_select_mode = "<A-t>",
-              switch_to_next_trail_mark_stack = "<A-.>",
-              switch_to_previous_trail_mark_stack = "<A-,>",
-              set_trail_mark_stack_sort_mode = "<A-S>",
-            },
+    keys = {
+      {
+        "<leader>tra",
+        "<cmd>TrailBlazerNewTrailMark<CR>",
+        desc = "TrailBlazerNewTrailMark",
+      },
+      {
+        "<leader>trd",
+        "<cmd>TrailBlazerTrackBack<CR>",
+        desc = "TrailBlazerTrackBack",
+      },
+      {
+        "<leader>trD",
+        "<cmd>TrailBlazerDeleteAllTrailMarks<CR>",
+        desc = "TrailBlazerDeleteAllTrailMarks",
+      },
+      {
+        "<leader>trD",
+        "<cmd>TrailBlazerDeleteAllTrailMarks<CR>",
+        desc = "TrailBlazerDeleteAllTrailMarks",
+      },
+      {
+        "<leader>trj",
+        "<cmd>TrailBlazerPeekMovePreviousUp<CR>",
+        desc = "TrailBlazerPeekMovePreviousUp",
+      },
+      {
+        "<leader>trk",
+        "<cmd>TrailBlazerPeekMoveNextDown<CR>",
+        desc = "TrailBlazerPeekMoveNextDown",
+      },
+      {
+        "<leader>trm",
+        "<cmd>TrailBlazerToggleTrailMarkList<CR>",
+        desc = "TrailBlazerToggleTrailMarkList",
+      },
+      {
+        "<leader>trp",
+        "<cmd>TrailBlazerPasteAtLastTrailMark<CR>",
+        desc = "TrailBlazerPasteAtLastTrailMark",
+      },
+    },
+    opts = {
+      mappings = { -- rename this to "force_mappings" to completely override default mappings and not merge with them
+        nv = { -- Mode union: normal & visual mode. Can be extended by adding i, x, ...
+          motions = {
+            new_trail_mark = "",
+            track_back = "",
+            peek_move_next_down = "",
+            peek_move_previous_up = "",
+            move_to_nearest = "",
+            toggle_trail_mark_list = "",
+          },
+          actions = {
+            delete_all_trail_marks = "",
+            paste_at_last_trail_mark = "",
+            paste_at_all_trail_marks = "",
+            set_trail_mark_select_mode = "",
+            switch_to_next_trail_mark_stack = "",
+            switch_to_previous_trail_mark_stack = "",
+            set_trail_mark_stack_sort_mode = "",
           },
         },
-        quickfix_mappings = { -- rename this to "force_quickfix_mappings" to completely override default mappings and not merge with them
-          -- nv = {
-          -- 	motions = {
-          -- 		qf_motion_move_trail_mark_stack_cursor = "<CR>",
-          -- 	},
-          -- 	actions = {
-          -- 		qf_action_delete_trail_mark_selection = "d",
-          -- 		qf_action_save_visual_selection_start_line = "v",
-          -- 	},
-          -- 	alt_actions = {
-          -- 		qf_action_save_visual_selection_start_line = "V",
-          -- 	},
-          -- },
-          -- v = {
-          -- 	actions = {
-          -- 		qf_action_move_selected_trail_marks_down = "<C-j>",
-          -- 		qf_action_move_selected_trail_marks_up = "<C-k>",
-          -- 	},
-          -- },
-        },
-      })
-    end,
+      },
+    },
   },
 }
