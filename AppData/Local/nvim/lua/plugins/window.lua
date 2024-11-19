ab4887c277e1e8108ac432ae36e87f655bd01675
felix <Wu-Felix@outlook.com>
2024-11-19 18:48:28 +0800

    refactor(nvim):nvim-window-picker 按lazyvim规格写配置

--- AppData/Local/nvim/lua/plugins/window.lua
+++ AppData/Local/nvim/lua/plugins/window.lua
@@ -2,47 +2,36 @@
 return {
   {
     "s1n7ax/nvim-window-picker",
-    lazy = false,
     event = { "WinNew" },
-    config = function()
-      local picker = require("window-picker")
-      picker.setup(
+    keys = {
+      {
+        "<leader>wj",
+        function()
+          local picked_window_id = require("window-picker").pick_window({
+            include_current_win = true,
+          }) or vim.api.nvim_get_current_win()
+          vim.api.nvim_set_current_win(picked_window_id)
+        end,
+        { desc = "Pick a window" },
         {
-          hint = 'floating-big-letter'
-        }
-      --   {
-      --   autoselect_one = true,
-      --   include_current = false,
-      --   filter_rules = {
-      --     bo = {
-      --       filetype = { "neo-tree", "neo-tree-popup", "notify", "quickfix" },
-      --       buftype = { "terminal" },
-      --     },
-      --   },
-      --   other_win_hl_color = "#e35e4f",
-      -- }
-      )
+          "<leader>wJ",
+          function()
+            local window = require("window-picker").pick_window({
+              include_current_win = false,
+            })
+            local target_buffer = vim.fn.winbufnr(window)
+            -- Set the target window to contain current buffer
+            vim.api.nvim_win_set_buf(window, 0)
+            -- Set current window to contain target buffer
+            vim.api.nvim_win_set_buf(0, target_buffer)
+          end,
+          { desc = "Swap windows" },
+        },
+      },
+    },
+    opts = {
 
-      vim.keymap.set("n", "<leader>wj", function()
-        local picked_window_id = picker.pick_window({
-          include_current_win = true,
-        }) or vim.api.nvim_get_current_win()
-        vim.api.nvim_set_current_win(picked_window_id)
-      end, { desc = "Pick a window" })
-
-      -- Swap two windows using the awesome window picker
-      local function swap_windows()
-        local window = picker.pick_window({
-          include_current_win = false,
-        })
-        local target_buffer = vim.fn.winbufnr(window)
-        -- Set the target window to contain current buffer
-        vim.api.nvim_win_set_buf(window, 0)
-        -- Set current window to contain target buffer
-        vim.api.nvim_win_set_buf(0, target_buffer)
-      end
-
-      vim.keymap.set("n", "<leader>wJ", swap_windows, { desc = "Swap windows" })
-    end,
+      hint = "floating-big-letter",
+    },
   },
 }
