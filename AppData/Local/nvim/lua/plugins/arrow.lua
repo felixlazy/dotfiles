04242be172b269475feb6d8f1ae5cd5f73f3a7ec
felix <Wu-Felix@outlook.com>
2024-11-22 15:57:32 +0800

    feat:arrow

---
+++ AppData/Local/nvim/lua/plugins/arrow.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "otavioschwanck/arrow.nvim",
+    dependencies = {
+      { "echasnovski/mini.icons" },
+    },
+    keys = {
+      ";",
+    },
+    opts = {
+      show_icons = true,
+      leader_key = ";", -- Recommended to be a single key
+      buffer_leader_key = "m", -- Per Buffer Mappings
+    },
+  },
+}
