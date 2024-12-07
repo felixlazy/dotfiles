c28387f1cc1920b7d40a20a85faccc3b168c64fe
felix <Wu-Felix@outlook.com>
2024-12-07 13:29:53 +0800

    refactor(nvim):various_textobjs

--- AppData/Local/nvim/lua/plugins/various_textobjs.lua
+++ AppData/Local/nvim/lua/plugins/various_textobjs.lua
@@ -2,18 +2,11 @@
 return {
   {
     "chrisgrieser/nvim-various-textobjs",
-    config = function()
-      require("various-textobjs").setup({
-        useDefaultKeymaps = true,
-        lookForwardLines = 10,
-      })
-      -- example: `an` for outer subword, `in` for inner subword
-      vim.keymap.set({ "o", "x" }, "aS", function()
-        require("various-textobjs").subword(false)
-      end)
-      vim.keymap.set({ "o", "x" }, "iS", function()
-        require("various-textobjs").subword(true)
-      end)
-    end,
+    event = "VeryLazy",
+    opts = {
+      keymaps = {
+        useDefaults = true,
+      },
+    },
   },
 }
