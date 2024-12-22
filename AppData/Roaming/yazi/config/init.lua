167c04ac32e4efc4ac2637e4ce1d36521f68fa3d
felix <Wu-Felix@outlook.com>
2024-12-22 11:07:27 +0800

    refactor(yazi):del relativ-motions

--- AppData/Roaming/yazi/config/init.lua
+++ AppData/Roaming/yazi/config/init.lua
@@ -15,4 +15,3 @@ require("yamb"):setup({
 		or (os.getenv("HOME") .. "/.config/yazi/bookmark"),
 })
 require("starship"):setup()
-require("relative-motions"):setup({ show_numbers = "relative", show_motion = true })
