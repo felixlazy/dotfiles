ec1a516f4d1666baf24912da75b717975d114f7d
felix <Wu-Felix@outlook.com>
2024-12-24 14:32:33 +0800

    feat(yazi):yaziline compress

--- AppData/Roaming/yazi/config/init.lua
+++ AppData/Roaming/yazi/config/init.lua
@@ -15,3 +15,10 @@ require("yamb"):setup({
 		or (os.getenv("HOME") .. "/.config/yazi/bookmark"),
 })
 require("starship"):setup()
+require("yaziline"):setup({
+	separator_style = "curvy",
+	select_symbol = "",
+	yank_symbol = "󰆐",
+	filename_max_length = 24, -- trim when filename > 24
+	filename_trim_length = 6, -- trim 6 chars from both ends
+})
