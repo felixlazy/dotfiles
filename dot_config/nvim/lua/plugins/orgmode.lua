761150f018a7f07edd2b80808022197207eac6e4
felix <Wu-Felix@outlook.com>
2025-01-01 18:45:04 +0800

    feat(nvim):add org-roam

--- dot_config/nvim/lua/plugins/orgmode.lua
+++ dot_config/nvim/lua/plugins/orgmode.lua
@@ -4,7 +4,11 @@ return {
     event = "VeryLazy",
     ft = { "org" },
     opts = {
-      org_agenda_files = "~/OneDrive/orgfiles" .. "/**/*",
+      org_agenda_files = {
+        "~/OneDrive/orgfiles/work/*.org",
+        "~/OneDrive/orgfiles/*.org",
+        "~/OneDrive/orgfiles/private/*.org",
+      },
       org_default_notes_file = "~/OneDrive/orgfiles" .. "/refile.org",
       org_todo_keywords = { "TODO(t)", "NEXT", "WAITING", "BUG", "|", "CANCEL", "DONE" },
       ui = {
@@ -42,6 +46,35 @@ return {
     opts = {
       markdown = {
         headline_highlights = false,
+      },
+    },
+  },
+  {
+    "chipsenkbeil/org-roam.nvim",
+    keys = {
+      { "<leader>n" },
+    },
+    opts = {
+      directory = "~/OneDrive/orgfiles/org_roam_files",
+      -- optional
+      org_files = {
+        "~/OneDrive/orgfiles/another_org_dir",
+        "~/OneDrive/orgfiles/some/folder/*.org",
+        "~/OneDrive/orgfiles/a/single/org_file.org",
+      },
+    },
+  },
+  {
+    "saghen/blink.cmp",
+    opts = {
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer", "orgmode" },
+        providers = {
+          orgmode = {
+            name = "Orgmode",
+            module = "orgmode.org.autocompletion.blink",
+          },
+        },
       },
     },
   },
--- dot_config/nvim/lua/utils/config.lua
+++ dot_config/nvim/lua/utils/config.lua
@@ -2,4 +2,4 @@ vim.cmd("hi Visual gui=reverse")
 vim.cmd("highlight CursorLine gui=NONE guibg=bg")
 -- vim.cmd("highlight Cursor gui=NONE guifg=bg guibg=#ffb6c1")
 -- add additional keyword chars
-vim.cmd([[autocmd FileType org setlocal iskeyword+=:,#,+]])
+-- vim.cmd([[autocmd FileType org setlocal iskeyword+=:,#,+]])
