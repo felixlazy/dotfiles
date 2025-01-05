5a26ed13bddf5dc9b880c8e8942af719554db996
felix <Wu-Felix@outlook.com>
2025-01-05 12:15:01 +0800

    refactor(nvim):根据终端决定是否启动smear_cursor

---
+++ dot_config/nvim/lua/plugins/smear_cursor.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "sphamba/smear-cursor.nvim",
+    cond = function()
+      if vim.g.neovide then
+        return false
+      end
+      if os.getenv("TERM") ~= "xterm-kitty" then
+        return true
+      else
+        return false
+      end
+    end,
+    opts = { -- Default  Range
+      stiffness = 0.8, -- 0.6      [0, 1]
+      trailing_stiffness = 0.5, -- 0.3      [0, 1]
+      distance_stop_animating = 0.5, -- 0.1      > 0
+      hide_target_hack = false, -- true     boolean
+      cursor_color = "#c8d3f5",
+    },
+  },
+  {
+    "echasnovski/mini.animate",
+    optional = true,
+    cond = vim.g.neovide == nil,
+    opts = {
+      cursor = { enable = false },
+    },
+  },
+}
