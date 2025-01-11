acd9a84c361a443b8aad4e80f4fbd9e15e672cca
felix <Wu-Felix@outlook.com>
2025-01-11 20:21:42 +0800

    feat(nvim):debugprint

---
+++ dot_config/nvim/lua/plugins/debugprint.lua
@@ -0,0 +1,9 @@
+return {
+  "andrewferrier/debugprint.nvim",
+  dependencies = {
+    "echasnovski/mini.nvim",
+  },
+  key = { "g?" },
+  version = "*", -- Remove if you DON'T want to use the stable version
+  opts = {},
+}
