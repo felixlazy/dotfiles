2a7755168fcff2fbefb672ce39a4af992b5ba83f
felix <Wu-Felix@outlook.com>
2025-01-22 13:05:13 +0800

    feat(nvim):img_clip

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -1,20 +1,34 @@
 return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  opts = {
-    code = {
-      sign = true,
-      width = "block",
-      right_pad = 1,
-    },
-    heading = {
-      sign = true,
-      icons = { "󰎥  ", "󰎨  ", "󰎫  ", "󰎲  ", "󰎯  ", "󰎴  " },
-      position = "inline",
-    },
-    checkbox = {
-      custom = {
-        important = { raw = "[>]", rendered = "", highlight = "DiagnosticWarn" },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      code = {
+        sign = true,
+        width = "block",
+        right_pad = 1,
       },
+      heading = {
+        sign = true,
+        icons = { "󰎥  ", "󰎨  ", "󰎫  ", "󰎲  ", "󰎯  ", "󰎴  " },
+        position = "inline",
+      },
+      checkbox = {
+        custom = {
+          important = { raw = "[>]", rendered = "", highlight = "DiagnosticWarn" },
+        },
+      },
+    },
+  },
+  {
+    "HakonHarnes/img-clip.nvim",
+    ft = "markdown",
+    opts = {
+      -- add options here
+      -- or leave it empty to use the default settings
+    },
+    keys = {
+      -- suggested keymap
+      { "<leader>mp", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
     },
   },
 }
