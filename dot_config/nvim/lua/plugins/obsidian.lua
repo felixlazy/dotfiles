74d9bb3c64d13d83368cae6a6f058b05a478cea6
felix <Wu-Felix@outlook.com>
2025-02-08 14:18:41 +0800

    feat(nvim):obsidian fzf_lua rend-markdown

--- dot_config/nvim/lua/plugins/obsidian.lua
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -5,7 +5,12 @@ return {
     ft = "markdown",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "ibhagwan/fzf-lua",
+      {
+        "ibhagwan/fzf-lua",
+        config = function()
+          require("render-markdown.core.ui")
+        end,
+      },
     },
     keys = {
       {
