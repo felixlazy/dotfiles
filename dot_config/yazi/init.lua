3723cee0a0ce99997082f872184e7a896db29d73
felix <Wu-Felix@outlook.com>
2025-03-12 18:58:15 +0800

    feat(yazi): add project management and file copy plugins
    - Added project management plugin with configuration for saving,
    loading,
      deleting, and merging projects.
    - Added key mappings for project management actions.
    - Added file copy plugin with configuration and key mapping.
    - Updated plugin dependencies in package.toml.

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -1,18 +1,13 @@
--- require("relative-motions"):setup({ show_numbers = "relative", show_motion = true })
--- require("starship"):setup()
--- You can configure your bookmarks by lua language
-local bookmarks = {}
 require("git"):setup()
 require("yamb"):setup({
-	-- Optional, the path ending with path seperator represents folder.
-	bookmarks = bookmarks,
+	-- Optional, recieve notification everytime you jump.
+	jump_notify = true,
 	-- Optional, the cli of fzf.
 	cli = "fzf",
 	-- Optional, a string used for randomly generating keys, where the preceding characters have higher priority.
 	keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
 	-- Optional, the path of bookmarks
-	path = (ya.target_family() == "windows" and os.getenv("APPDATA") .. "\\yazi\\config\\bookmark")
-		or (os.getenv("HOME") .. "/.config/yazi/bookmark"),
+	path = os.getenv("HOME") .. "/.config/yazi/bookmark",
 })
 require("starship"):setup()
 require("yaziline"):setup({
@@ -21,4 +16,30 @@ require("yaziline"):setup({
 	yank_symbol = "󰆐",
 	filename_max_length = 24, -- trim when filename > 24
 	filename_trim_length = 6, -- trim 6 chars from both ends
+})
+require("projects"):setup({
+	save = {
+		method = "yazi", -- yazi | lua
+		lua_save_path = "", -- comment out to get the default value
+		-- windows: "%APPDATA%/yazi/state/projects.json"
+		-- unix: "~/.local/state/yazi/projects.json"
+	},
+	last = {
+		update_after_save = true,
+		update_after_load = true,
+		load_after_start = false,
+	},
+	merge = {
+		quit_after_merge = false,
+	},
+	notify = {
+		enable = true,
+		title = "Projects",
+		timeout = 3,
+		level = "info",
+	},
+})
+require("copy-file-contents"):setup({
+	append_char = "\n",
+	notification = true,
 })
