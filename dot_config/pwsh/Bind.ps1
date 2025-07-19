43ae6bc786188a925f226fe405994b081081f932
felix <Wu-Felix@outlook.com>
2025-07-19 13:54:12 +0800

    feat(pwsh): 增加命令glo gro genv - glo 通过fzf查看git log - gro 通过fzf查看git relog - genv 通过fzf查看env

--- dot_config/pwsh/Bind.ps1
+++ dot_config/pwsh/Bind.ps1
@@ -43,3 +43,25 @@ function mpvc
   )
   mpv --autofit=80%x80% --geometry=50%:50% @Files
 }
+function grl
+{
+  git reflog --pretty='%h %gs' | 
+    fzf --ansi --no-sort --preview "git show -p --stat --pretty=fuller --color=always {1} | delta"
+}
+function glo
+{
+  git log --pretty=format:'%C(yellow)%h %Cgreen%ad %Cblue%an%Creset %s' --date=short |
+    fzf --ansi --no-sort --preview="git show --color=always {1} | delta" --bind "enter:execute:git show {1} | delta | less -R"
+}
+function gd
+{
+  git diff --name-only | fzf --ansi `
+    --preview 'git diff --color=always {} | delta' `
+    --bind 'enter:execute(nvim {})'
+}
+function fenv
+{
+  Get-ChildItem Env: | ForEach-Object { "$($_.Name)=$($_.Value)" } | 
+    fzf --preview 'powershell -c "echo $env:{1}"'
+}
+
