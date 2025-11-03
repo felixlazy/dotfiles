457fd3cdd7f0c0886cf25e006466ad544219dd03
felix <Wu-Felix@outlook.com>
2025-11-03 19:16:36 +0800

    refactor(pwsh,lazygit): 更新配置

--- dot_config/pwsh/Microsoft.PowerShell_profile.ps1
+++ dot_config/pwsh/Microsoft.PowerShell_profile.ps1
@@ -32,9 +32,8 @@ $Env:https_proxy="http://127.0.0.1:7890"
 . "$Env:XDG_CONFIG_HOME\fzf\Fzf.ps1"
 . "$Env:XDG_CONFIG_HOME\pwsh\Eza.ps1"
 . "$Env:XDG_CONFIG_HOME\pwsh\Bind.ps1"
-. "$Env:XDG_CONFIG_HOME\pwsh\Onefetch.ps1"
+# . "$Env:XDG_CONFIG_HOME\pwsh\Onefetch.ps1"
 . "$Env:XDG_CONFIG_HOME\pwsh\Chezmoi.ps1"
-. "$Env:XDG_CONFIG_HOME\pwsh\aichat.ps1"
 . "$Env:XDG_CONFIG_HOME\pwsh\Env.ps1"
 
 Import-Module PSCompletions
