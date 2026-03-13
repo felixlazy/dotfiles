3fa9623ecbd254952ed680762fe1242f8affb37b
felix <Wu-Felix@outlook.com>
2026-03-13 20:13:14 +0800

    refactor(chezmoi): 去除加密

--- dot_config/pwsh/Microsoft.PowerShell_profile.ps1
+++ dot_config/pwsh/Microsoft.PowerShell_profile.ps1
@@ -34,7 +34,6 @@ $Env:https_proxy="http://127.0.0.1:7890"
 . "$Env:XDG_CONFIG_HOME\pwsh\Eza.ps1"
 . "$Env:XDG_CONFIG_HOME\pwsh\Bind.ps1"
 . "$Env:XDG_CONFIG_HOME\pwsh\Chezmoi.ps1"
-. "$Env:XDG_CONFIG_HOME\pwsh\Env.ps1"
 . "$Env:XDG_CONFIG_HOME\procs\Procs.ps1"
 
 Import-Module PSCompletions
